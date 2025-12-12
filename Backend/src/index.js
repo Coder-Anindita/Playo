bbdb0aa063a921bb9c23a739b42f280ca77ff446
Anindita Chakraborty <aninditac1912@gmail.com>
2025-12-12 11:00:19 +0530

    add utils and other setups

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,22 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 import express from "express"
+import CORS_ORIGIN from ""
 const app=express()
 dotenv.config({
     path:"./env"
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Database Connection error",err);
+    
+
+})
 
 
 
---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+const asyncHandler=(fn)=>async(rq,res,next)=>{
+    try{
+        await fn(req,res,next)
+    }
+    catch(err){
+        res.status(err.code || 500).json({
+            success:false,
+            message:err.message
+        })
+    }
+
+}
+export default asyncHandler
